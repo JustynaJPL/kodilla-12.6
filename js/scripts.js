1c7b389e32f0dacade7494dba9ad0187aef90895
Jan Kowalski <justyna.jarkiewiczapk@gmail.com>
2018-01-04 15:07:53 +0100

    12.6 - draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
 $(function() {
-    
+    var url = 'https://restcountries.eu/rest/v1/name/';
+    var countriesList = $('#countries');
+    var countryNameinp = $('#country-name');
+
+    $('#search').click(searchCountries);
+    $('#country-name').focus(clearImput);
+
+    function searchCountries() {    	
+        
+        var countryName = $('#country-name').val();
+        if (!countryName.length) countryName = 'Poland';
+        $.ajax({
+            url: url + countryName,
+            method: 'GET',
+            success: showCountriesList
+        });
+    }
+
+    function showCountriesList(resp) {
+        countriesList.empty();
+
+        resp.forEach(function(item) {
+        	$('<li>').text(item.name).appendTo(countriesList);
+    	});
+    }
+    function clearImput(){
+    	countryNameinp.val("");
+    }
+
 });
